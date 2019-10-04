23d17010d877bcc0f2e48b88b7d40e79a7610e1c
Alex Sidorov <promoabys@gmail.com>
2019-10-04 03:58:41 +0300

    inital: Add solve of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,49 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor(min, max, result) {        
+        this.min = min;
+        this.max = max;
+        this.result = result;
     }
 
-    guess() {
+    setRange(min, max) {      
 
+        this.min = min;
+        this.max = max;
+        this.result = (max-min)/2;
+
+        // console.log('SetRange result = ' + this.result);   
+        // console.log('SetRange Min Max = ' + this.min, this.max);    
+        // console.log('');
+    }
+
+    guess() {   
+       
+        // console.log('Guess result = ' + this.result);   
+        // console.log('');  
+        this.result = Math.round(this.result);
+
+        return this.result;
     }
 
     lower() {
+
+        this.max = this.result;
+        this.result = this.result - (this.max-this.min)/2;
+
+        // console.log('Lower result finish = ' + this.result);   
+        // console.log('Lower Min Max finish = ' + this.min, this.max);  
+        // console.log('');
 
     }
 
     greater() {
 
+        this.min = this.result;
+        this.result = this.result + (this.max-this.min)/2;
+
+        // console.log('Greater result finish = ' + this.result);   
+        // console.log('Greater Min Max finish = ' + this.min, this.max);  
+        // console.log('');
     }
 }
 
